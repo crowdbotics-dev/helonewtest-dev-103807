da85daed94fd8e6c4d9f810a2156c220da1f04a4
Crowdbotics <engineering@crowdbotics.com>
2023-07-21 10:28:27 +0000

    Initial commit.

---
+++ backend/helonewtest_dev_103807/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for helonewtest_dev_103807 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helonewtest_dev_103807.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "helonewtest-dev-103807",
+  url: "https://helonewtest-dev-103807.botics.co",
+  api: "https://helonewtest-dev-103807.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
